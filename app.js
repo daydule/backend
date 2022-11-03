4389976e12df61d331c231a5e59f63f1af336a56
Keisuke Nemura <nemuraystk@gmail.com>
2022-11-03 22:46:38 +0900

    add ログイン周りの雛形作成

--- app.js
+++ app.js
@@ -3,17 +3,38 @@
 const express = require('express');
 const app = express();
 const auth = require('./routes/auth');
-const preferences = require('./config/preferences');
-const port = preferences.port;
+// eslint-disable-next-line node/no-unpublished-require
+const secret = require('./config/secret');
+const port = secret.port;
+
+const pool = require('./db/pool');
 
 app.use('/', auth);
 
+// sample code start ----------
 app.get('/', (req, res) => {
-  res.json({
-    hello: 'daydule'
-  });
+    res.json({
+        hello: 'daydule'
+    });
 });
 
+app.get('/memo', function (req, res, next) {
+    // SELECT
+    pool.query('SELECT * FROM memo', function (error, results) {
+        // エラーの場合
+        if (error) {
+            throw error;
+        }
+
+        // 正常なら取得したデータを返却
+        res.status(200).json({
+            data: results.rows
+        });
+        return next();
+    });
+});
+// sample code end ----------
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`app listening on port ${port}`);
 });
--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,32 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/signup', (req, res, next) => {
+    // TODO バリデーションチェックを行う
+
+    // TODO メールアドレスチェック
+
+    // TODO ゲストユーザーチェック
+
+    // TODO サインアップ処理
+
+    // TODO 曜日別設定
+
+    res.json({
+        isError: false
+    });
+    return next();
+});
+
 router.post('/login', (req, res, next) => {
-  res.json({
-    isError: false
-  });
-  return next();
+    // TODO バリデーションチェックを行う
+
+    // TODO ログイン処理を行う
+
+    res.json({
+        isError: false
+    });
+    return next();
 });
 
 module.exports = router;
