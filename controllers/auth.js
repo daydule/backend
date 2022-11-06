b05f0dea5f4f12af05372228297d7d223b3f68aa
Keisuke Nemura <nemuraystk@gmail.com>
2022-11-06 17:47:59 +0900

    レスポンスにHTTPステータスコードが設定されるように修正

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,47 +11,61 @@ const pool = require('../db/pool');
  * サインアップ
  */
 router.post('/signup', async (req, res) => {
-    const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
     const salt = crypto.randomBytes(16).toString('base64');
+    let isClientError = false;
 
     try {
         // TODO バリデーションチェックを行う
 
         const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
         if (result.rows.length > 0) {
+            isClientError = true;
             throw new Error('そのメールアドレスは既に使用されています。');
         }
 
         if (req.user && req.user.is_guest) {
             // ゲストのサインアップ処理
             const hashedPassword = await promisify(crypto.pbkdf2)(password, salt, 310000, 32, 'sha256');
-            await pool.query(
-                'UPDATE users SET user_name = $1, email = $2, password = $3, salt = $4, is_guest = $5 WHERE id = $6',
-                [username, email, hashedPassword.toString('base64'), salt, false, req.user.id]
-            );
+            await pool.query('UPDATE users SET email = $1, password = $2, salt = $3, is_guest = $4 WHERE id = $5', [
+                email,
+                hashedPassword.toString('base64'),
+                salt,
+                false,
+                req.user.id
+            ]);
         } else {
             // ゲスト以外のサインアップ処理
             const hashedPassword = await promisify(crypto.pbkdf2)(password, salt, 310000, 32, 'sha256');
-            await pool.query(
-                'INSERT INTO users (user_name, email, password, salt, is_guest) VALUES ($1, $2, $3, $4, $5)',
-                [username, email, hashedPassword.toString('base64'), salt, false]
-            );
+            await pool.query('INSERT INTO users (email, password, salt, is_guest) VALUES ($1, $2, $3, $4)', [
+                email,
+                hashedPassword.toString('base64'),
+                salt,
+                false
+            ]);
         }
 
         // TODO 曜日別設定を行う
 
-        res.json({
+        return res.status(200).json({
             isError: false
         });
     } catch (e) {
         console.error(e);
-        res.json({
-            isError: true,
-            errorCode: 'errorCode',
-            errorMessage: 'サインアップエラー'
-        });
+        if (isClientError) {
+            return res.status(400).json({
+                isError: true,
+                errorId: 'errorId',
+                errorMessage: 'サインアップエラー'
+            });
+        } else {
+            return res.status(500).json({
+                isError: true,
+                errorId: 'errorId',
+                errorMessage: 'サインアップエラー'
+            });
+        }
     }
 });
 
@@ -59,7 +73,7 @@ router.post('/signup', async (req, res) => {
  * ログイン
  */
 router.post('/login', passport.authenticate('local', { failureRedirect: '/authError' }), (req, res) => {
-    res.json({
+    res.status(200).json({
         isError: false
     });
 });
@@ -68,9 +82,9 @@ router.post('/login', passport.authenticate('local', { failureRedirect: '/authEr
  * 認証エラー時のレスポンスを返却する
  */
 router.get('/authError', (req, res) => {
-    res.json({
+    res.status(400).json({
         isError: true,
-        errorCode: 'errorCode',
+        errorId: 'errorId',
         errorMessage: '認証エラー'
     });
 });
