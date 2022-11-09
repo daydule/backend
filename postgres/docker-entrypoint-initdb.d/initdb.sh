fcf17cf4b873edf3bfe1f8bbc13eb9fc550b414e
tom-takeru <tom.2461.ty@gmail.com>
2022-11-09 18:57:51 +0900

    fix 不要なコメントアウトをinitdb.shから削除

--- postgres/docker-entrypoint-initdb.d/initdb.sh
+++ postgres/docker-entrypoint-initdb.d/initdb.sh
@@ -1,9 +1,5 @@
 CMD_PSQL="psql -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" -p "${POSTGRES_PORT}" -f "
 
-# connect_pg_simple_sql=/home/daydule/connect-pg-simple/table.sql
-# echo runnning $connect_pg_simple_sql
-# $CMD_PSQL $connect_pg_simple_sql
-
 for dir in /home/daydule/src/*; do
   for sql in $dir/*.sql; do
     if [ -e $sql ]; then
