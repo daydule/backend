5c20c5d1ceeb9740ed11d59ece44d3b598b4d82f
Keisuke Nemura <nemuraystk@gmail.com>
2022-11-11 22:57:52 +0900

    update エラーハンドリング、作成/更新結果レコードを返却するように修正

--- controllers/plan.js
+++ controllers/plan.js
@@ -9,24 +9,43 @@ const pool = require('../db/pool');
  */
 router.post('/upsertTodoPriority', async (req, res) => {
     const todoOrders = req.body.todoOrders; // TODOのIDをカンマ区切りにした文字列
+    let upsertResult;
 
-    // TODO バリデーションチェックを行う
+    try {
+        // TODO バリデーションチェックを行う
 
-    // TODO並び順の取得（履歴用ではなく、ユーザーに一つだけ紐づく並び順を取得）
-    const result = await pool.query('SELECT * FROM todo_orders WHERE user_id = $1 AND schedule_id IS NULL', [
-        req.user.id
-    ]);
+        // TODO並び順の取得（履歴用ではなく、ユーザーに一つだけ紐づく並び順を取得）
+        const getResult = await pool.query('SELECT * FROM todo_orders WHERE user_id = $1 AND schedule_id IS NULL', [
+            req.user.id
+        ]);
 
-    if (result.rows.length > 0) {
-        await pool.query('UPDATE todo_orders SET todo_orders = $1 WHERE id = $2', [todoOrders, result.rows[0].id]);
-    } else {
-        await pool.query('INSERT INTO todo_orders (user_id, todo_orders) VALUES ($1, $2)', [req.user.id, todoOrders]);
+        if (getResult.rows.length > 0) {
+            upsertResult = await pool.query('UPDATE todo_orders SET todo_orders = $1 WHERE id = $2 RETURNING *', [
+                todoOrders,
+                getResult.rows[0].id
+            ]);
+        } else {
+            upsertResult = await pool.query(
+                'INSERT INTO todo_orders (user_id, todo_orders) VALUES ($1, $2) RETURNING *',
+                [req.user.id, todoOrders]
+            );
+        }
+
+        return res.status(200).json({
+            isError: false,
+            todoOrders: upsertResult.rows[0]
+        });
+    } catch (e) {
+        console.error(e);
+
+        // TODO バリデーションエラーはHTTPステータスコード400で返却する
+
+        return res.status(500).json({
+            isError: true,
+            errorId: 'errorId',
+            errorMessage: 'システムエラー'
+        });
     }
-
-    return res.status(200).json({
-        isError: false,
-        todoOrders: todoOrders
-    });
 });
 
 module.exports = router;
