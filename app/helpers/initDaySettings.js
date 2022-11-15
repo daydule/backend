1d8b598c891a42550409ac09b1de412139f25910
tom-takeru <tom.2461.ty@gmail.com>
2022-11-15 22:55:21 +0900

    fix 変数名とjsDocの型の定義を正しく修正

--- app/helpers/initDaySettings.js
+++ app/helpers/initDaySettings.js
@@ -1,19 +1,21 @@
 'use strict';
 
+const { Pool } = require('pg');
+
 /**
  * 曜日別設定の初期設定をする
  *
- * @param {import("pg").PoolClient} client - DB接続
+ * @param {Pool} pool - DB接続
  * @param {string} userId - 初期設定するユーザーのID
  * @returns {void}
  */
-async function initDaySettings(client, userId) {
+async function initDaySettings(pool, userId) {
     const dayList = ['日', '月', '火', '水', '木', '金', '土'];
     for (let i = 0; i < dayList.length; i++) {
         const sql =
             'INSERT INTO day_settings (user_id, setting_name, day, schedule_start_time, schedule_end_time, scheduling_logic) VALUES ($1, $2, $3, $4, $5, $6)';
         const params = [userId, dayList[i] + '曜日', i, '09:00', '18:00', 0];
-        await client.query(sql, params);
+        await pool.query(sql, params);
     }
 }
 
