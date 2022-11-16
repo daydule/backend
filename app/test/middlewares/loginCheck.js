3b67c429f5fbb7e42ac9028abd23c577fac72ce0
tom-takeru <tom.2461.ty@gmail.com>
2022-11-16 22:03:18 +0900

    add [ログインチェック]正常時の単体テストを追加

--- app/test/middlewares/loginCheck.js
+++ app/test/middlewares/loginCheck.js
@@ -2,11 +2,14 @@
 
 // eslint-disable-next-line node/no-unpublished-require
 const assert = require('chai').assert;
+// eslint-disable-next-line node/no-unpublished-require
+const sinon = require('sinon');
 const loginCheck = require('../../middlewares/loginCheck');
 
 describe('loginCheck.js', function () {
     describe('loginCheck function', function () {
         it('should return json of error if not logged in.', function () {
+            const next = sinon.stub().callsFake(function () {});
             const res = {
                 status: function (code) {
                     return {
@@ -16,8 +19,27 @@ describe('loginCheck.js', function () {
                     };
                 }
             };
-            const result = loginCheck({}, res, function () {});
+            const result = loginCheck({}, res, next);
             assert.equal(result.isError, true);
+            assert.equal(next.callCount, 0);
+        });
+
+        it('should call next function once if logged in.', function () {
+            const req = {
+                user: {}
+            };
+            const res = {
+                status: function (code) {
+                    return {
+                        json: function (json) {
+                            return json;
+                        }
+                    };
+                }
+            };
+            const next = sinon.stub().callsFake(function () {});
+            loginCheck(req, res, next);
+            assert.equal(next.callCount, 1);
         });
     });
 });
