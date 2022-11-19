86adcbe047f20025418856e5a4cb62a425a96219
tom-takeru <tom.2461.ty@gmail.com>
2022-11-19 18:15:17 +0900

    add [曜日別設定]単体テストを追加

---
+++ app/test/helpers/daySettingsHelper.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// eslint-disable-next-line node/no-unpublished-require
+const assert = require('chai').assert;
+const { Pool } = require('pg');
+// eslint-disable-next-line node/no-unpublished-require
+const sinon = require('sinon');
+const daySettingsHelper = require('../../helpers/daySettingsHelper');
+
+describe('daySettingsHelper.js', function () {
+    describe('initDaySettings function', function () {
+        it('should call pool.query function 7 times.', async function () {
+            const stub = sinon.stub().returns({ rows: ['dummy'] });
+            const pool = { query: stub };
+            await daySettingsHelper.initDaySettings(pool, 'dummyUserId');
+            assert(stub.callCount, 7);
+        });
+    });
+});
