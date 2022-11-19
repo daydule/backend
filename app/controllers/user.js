06dd5bd594096309a6eb57e08ce9e7e93960eea6
tom-takeru <tom.2461.ty@gmail.com>
2022-11-19 22:53:30 +0900

    fix [ユーザー情報更新]ユーザー情報更新ではパスワード以外の更新に変更

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -2,9 +2,9 @@
 
 const express = require('express');
 const router = express.Router();
+const pool = require('../db/pool');
 const { promisify } = require('util');
 const crypto = require('crypto');
-const pool = require('../db/pool');
 
 /**
  * ユーザー情報参照
@@ -24,25 +24,43 @@ router.get('/read', function (req, res) {
  * ユーザー情報更新
  */
 router.post('/update', async function (req, res) {
+    // TODO: ゲストチェックのミドルウェアを更新したらそっちを使う
+    if (req.user.is_guest) {
+        console.error('ゲストユーザーがユーザー情報更新(/user/update)を呼び出しました。');
+        return res.status(400).json({
+            isError: true,
+            errorId: 'errorId',
+            errorMessage: 'ゲストユーザーはユーザー情報を更新できません。'
+        });
+    }
+
     const userName = req.body.userName;
     const email = req.body.email;
     const password = req.body.password;
-    const salt = crypto.randomBytes(16).toString('base64');
 
     try {
-        // TODO: バリデーションチェックする
+        // TODO: バリデーションチェック（書式チェック）をする
 
-        const hashedPassword = await promisify(crypto.pbkdf2)(password, salt, 310000, 32, 'sha256');
-        const result = await pool.query(
-            'UPDATE users SET user_name = $1, email = $2, password = $3, salt = $4 WHERE id = $5 RETURNING *',
-            [userName, email, hashedPassword.toString('base64'), salt, req.user.id]
-        );
+        // TODO: 以下のバリデーションチェック（パスワードが正しいかどうかのチェック）をヘルパーで実装する
+        // バリデーションチェック（パスワードが正しいかどうかのチェック）をする
+        const hashedPassword = await promisify(crypto.pbkdf2)(password, req.user.salt, 310000, 32, 'sha256');
+        if (hashedPassword.toString('base64') != req.user.password) {
+            console.error('パスワードが違います。');
+            return res.status(400).json({
+                isError: true,
+                errorId: 'errorId',
+                errorMessage: 'パスワードが違います。'
+            });
+        }
+
+        const sql = 'UPDATE users SET user_name = $1, email = $2 WHERE id = $3 RETURNING *';
+        const values = [userName, email, req.user.id];
+        const result = await pool.query(sql, values);
         return res.status(200).json({
             isError: false,
             user: {
                 userName: result.rows[0].user_name,
-                email: result.rows[0].email,
-                isGuest: result.rows[0].is_guest
+                email: result.rows[0].email
             }
         });
     } catch (e) {
