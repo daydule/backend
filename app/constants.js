8ed463528391003e60d48e03069080927d852ff5
tom-takeru <tom.2461.ty@gmail.com>
2022-11-19 18:09:28 +0900

    fix [曜日別設定]曜日を定数ファイルに移動

--- app/constants.js
+++ app/constants.js
@@ -6,5 +6,6 @@ module.exports = {
             SCHEDULE_START_TIME: '0900',
             SCHEDULE_END_TIME: '1800'
         }
-    }
+    },
+    DAY_LIST: ['日', '月', '火', '水', '木', '金', '土']
 };
--- app/helpers/daySettingsHelper.js
+++ app/helpers/daySettingsHelper.js
@@ -2,6 +2,7 @@
 
 const { Pool } = require('pg');
 const DEFAULT = require('../constants').DEFAULT;
+const DAY_LIST = require('../constants').DAY_LIST;
 
 /**
  * 曜日別設定の初期設定をする
@@ -11,15 +12,14 @@ const DEFAULT = require('../constants').DEFAULT;
  * @returns {object} - 設定をした個別曜日レコード
  */
 async function initDaySettings(pool, userId) {
-    const dayList = ['日', '月', '火', '水', '木', '金', '土'];
     const sql =
         'INSERT INTO day_settings (user_id, setting_name, day, schedule_start_time, schedule_end_time, scheduling_logic)' +
         'VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
     let daySettingList = [];
-    for (let i = 0; i < dayList.length; i++) {
+    for (let i = 0; i < DAY_LIST.length; i++) {
         const params = [
             userId,
-            dayList[i],
+            DAY_LIST[i],
             i,
             DEFAULT.DAY_SETTINGS.SCHEDULE_START_TIME,
             DEFAULT.DAY_SETTINGS.SCHEDULE_END_TIME,
