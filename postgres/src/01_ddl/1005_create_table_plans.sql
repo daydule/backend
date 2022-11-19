bac688e897f9cf6e99e60f1bdec8fc9259f58989
Mellbrother <56198902+Mellbrother@users.noreply.github.com>
2022-11-19 17:55:09 +0900

    DAYD-612 [実装]ACT-PLN-PlanCreate_予定TODO登録 (#7)
    * add 予定作成アクション実装
    
    * update 不要なコメント削除
    
    * update resultの定義位置修正
    
    Co-authored-by: Keisuke Nemura <nemuraystk@gmail.com>
    Co-authored-by: tom-takeru <tom.2461.ty@gmail.com>

---
+++ postgres/src/01_ddl/1005_create_table_plans.sql
@@ -0,0 +1,21 @@
+CREATE TABLE "plans"
+(
+  "id"               SERIAL       NOT NULL PRIMARY KEY,
+  "user_id"          INTEGER      NOT NULL REFERENCES "users" ("id"),
+  "title"            VARCHAR(100) NOT NULL, 
+  "context"          TEXT,
+  "date"             DATE         NOT NULL,
+  "start_time"       VARCHAR(4),
+  "end_time"         VARCHAR(4),
+  "process_time"     INTEGER,
+  "travel_time"      INTEGER,
+  "buffer_time"      INTEGER,
+  "plan_type"        INTEGER,
+  "priority"         INTEGER      NOT NULL,
+  "place"            VARCHAR(100),
+  "is_scheduled"     BOOLEAN      DEFAULT FALSE,
+  "is_required_plan" BOOLEAN,
+  "parent_plan_id"   INTEGER,
+  "is_parent_plan"   BOOLEAN,
+  "todo_start_time"  TIMESTAMP
+);
