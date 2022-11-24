d22b4467baa1280840a33f8338614be413b851c9
tom-takeru <tom.2461.ty@gmail.com>
2022-11-24 18:22:42 +0900

    update パスワード更新APIを追加

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -64,4 +64,53 @@ router.post('/update', guestCheck, async function (req, res) {
     }
 });
 
+/**
+ * パスワード更新
+ */
+router.post('/password/update', guestCheck, async function (req, res) {
+    const currentPassword = req.body.currentPassword;
+    const newPassword = req.body.newPassword;
+
+    try {
+        // TODO: バリデーションチェック（書式チェック）をする
+
+        // TODO: 以下のバリデーションチェック（パスワードが正しいかどうかのチェック）をヘルパーで実装する
+        // バリデーションチェック（パスワードが正しいかどうかのチェック）をする
+        const hashedCurrentPassword = await promisify(crypto.pbkdf2)(
+            currentPassword,
+            req.user.salt,
+            310000,
+            32,
+            'sha256'
+        );
+        if (hashedCurrentPassword.toString('base64') != req.user.password) {
+            console.error('パスワードが違います。');
+            return res.status(400).json({
+                isError: true,
+                errorId: 'errorId',
+                errorMessage: 'パスワードが違います。'
+            });
+        }
+
+        const hashedNewPassword = await promisify(crypto.pbkdf2)(newPassword, req.user.salt, 310000, 32, 'sha256');
+        const sql = 'UPDATE users SET password = $1 WHERE id = $2 RETURNING *';
+        const values = [hashedNewPassword.toString('base64'), req.user.id];
+        const result = await pool.query(sql, values);
+        return res.status(200).json({
+            isError: false,
+            user: {
+                userName: result.rows[0].user_name,
+                email: result.rows[0].email
+            }
+        });
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json({
+            isError: true,
+            errorId: 'errorId',
+            errorMessage: 'サーバーエラー'
+        });
+    }
+});
+
 module.exports = router;
