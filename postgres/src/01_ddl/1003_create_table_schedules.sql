f32832270a08d6bdebe00aa900949922fa781bd3
Keisuke Nemura <nemuraystk@gmail.com>
2022-11-28 20:31:58 +0900

    テーブル定義ファイル作成

--- postgres/src/01_ddl/1003_create_table_schedules.sql
+++ postgres/src/01_ddl/1003_create_table_schedules.sql
@@ -1,10 +1,11 @@
 CREATE TABLE "schedules"
 (
-  "id"                      SERIAL      NOT NULL PRIMARY KEY,
-  "date"                    DATE        NOT NULL,
-  "start_time"              VARCHAR(4)  NOT NULL DEFAULT '0900',
-  "end_time"                VARCHAR(4)  NOT NULL DEFAULT '1800',
+  "id"                      SERIAL             NOT NULL PRIMARY KEY,
+  "user_id"                 INT                NOT NULL REFERENCES "users" ("id"),
+  "date"                    DATE        UNIQUE NOT NULL,
+  "start_time"              VARCHAR(4)         NOT NULL DEFAULT '0900',
+  "end_time"                VARCHAR(4)         NOT NULL DEFAULT '1800',
   "start_time_at_schedule"  VARCHAR(4),
   "end_time_at_schedule"    VARCHAR(4),
-  "is_scheduled"            BOOLEAN     NOT NULL DEFAULT FALSE
+  "is_scheduled"            BOOLEAN            NOT NULL DEFAULT FALSE
 );
---
+++ postgres/src/01_ddl/1007_create_table_fix_plans.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "fix_plans"
+(
+  "id"               SERIAL       NOT NULL PRIMARY KEY,
+  "day_id"           INTEGER      NOT NULL REFERENCES "day_settings" ("id"),
+  "set_id"           INTEGER,
+  "title"            VARCHAR(100) NOT NULL, 
+  "context"          TEXT,
+  "start_time"       VARCHAR(4),
+  "end_time"         VARCHAR(4),
+  "process_time"     INTEGER,
+  "travel_time"      INTEGER,
+  "buffer_time"      INTEGER,
+  "plan_type"        INTEGER,
+  "priority"         INTEGER      NOT NULL,
+  "place"            VARCHAR(100)
+);
