c0b8a03c0655872303d30d03402bb2030fb73f1b
Keisuke Nemura <nemuraystk@gmail.com>
2022-11-28 23:11:24 +0900

    add 予定作成（スケジュール後）処理の実装

--- app/controllers/plan.js
+++ app/controllers/plan.js
@@ -67,6 +67,66 @@ router.post('/create', async (req, res) => {
 });
 
 /**
+ * 予定作成（スケジュール作成後）
+ */
+router.post('/temporary/create', async (req, res) => {
+    const userId = req.user.id;
+    const title = req.body.title;
+    const context = req.body.context;
+    const date = req.body.date;
+    const startTime = req.body.startTime;
+    const endTime = req.body.endTime;
+    const processTime = req.body.processTime;
+    const travelTime = req.body.travelTime;
+    const bufferTime = req.body.bufferTime;
+    const planType = req.body.planType;
+    const priority = req.body.priority;
+    const place = req.body.place;
+    const todoStartTime = req.body.todoStartTime;
+
+    try {
+        // TODO バリデーションチェックを行う
+
+        const result = await pool.query(
+            'INSERT INTO temporary_plans (\
+                user_id, title, context, date, start_time, end_time, process_time, \
+                travel_time, buffer_time, plan_type, priority, place, todo_start_time) \
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *',
+            [
+                userId,
+                title,
+                context,
+                date,
+                startTime,
+                endTime,
+                processTime,
+                travelTime,
+                bufferTime,
+                planType,
+                priority,
+                place,
+                todoStartTime
+            ]
+        );
+
+        return res.status(200).json({
+            isError: false,
+            temporaryPlan: result.rows[0]
+        });
+    } catch (e) {
+        // TODO バリデーションエラーはHTTPステータスコード400で返却するように実装する
+
+        console.error(e);
+
+        return res.status(500).json({
+            isError: true,
+            errorId: 'errorId',
+            errorMessage: 'システムエラー'
+        });
+    }
+});
+
+/**
  * TODO並び順の作成/更新処理
  */
 router.post('/upsertTodoPriority', async (req, res) => {
