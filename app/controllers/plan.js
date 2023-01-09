377dfa4320e53b1855634c4fa5c521aec84bcb83
tom-takeru <tom.2461.ty@gmail.com>
2023-01-09 16:09:25 +0900

    DAYD-639 add 予定作成APIとtodo並び順APIにバリデーションを追加

--- app/controllers/plan.js
+++ app/controllers/plan.js
@@ -3,79 +3,112 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db/pool');
+const { validationResult } = require('express-validator/check');
+const { plansValidationCheck, todoOrdersValidationCheck, errorMessageFormatter } = require('../middlewares/validator');
 
 /**
  * 予定作成
  */
-router.post('/create', async (req, res) => {
-    const userId = req.user.id;
-    const title = req.body.title;
-    const context = req.body.context;
-    const date = req.body.date;
-    const startTime = req.body.start_time;
-    const endTime = req.body.end_time;
-    const processTime = req.body.process_time;
-    const travelTime = req.body.travel_time;
-    const bufferTime = req.body.buffer_time;
-    const planType = req.body.plan_type;
-    const priority = req.body.priority;
-    const place = req.body.place;
-    const isRequiredPlan = req.body.is_required_plan;
-    const todoStartTime = req.body.todo_start_time;
+router.post(
+    '/create',
+    [
+        plansValidationCheck.title,
+        plansValidationCheck.context,
+        plansValidationCheck.date,
+        plansValidationCheck.startTime,
+        plansValidationCheck.endTime,
+        plansValidationCheck.processTime,
+        plansValidationCheck.travelTime,
+        plansValidationCheck.bufferTime,
+        plansValidationCheck.planType,
+        plansValidationCheck.priority,
+        plansValidationCheck.place,
+        plansValidationCheck.isRequiredPlan,
+        plansValidationCheck.todoStartTime
+    ],
+    async (req, res) => {
+        const result = validationResult(req);
+        if (result.errors.length !== 0) {
+            console.error(result);
+            return res.status(400).json({
+                isError: true,
+                errorId: 'errorId',
+                errorMessage: errorMessageFormatter(result.errors)
+            });
+        }
 
-    try {
-        // TODO バリデーションチェックを行う
+        const userId = req.user.id;
+        const title = req.body.title;
+        const context = req.body.context;
+        const date = req.body.date;
+        const startTime = req.body.startTime;
+        const endTime = req.body.endTime;
+        const processTime = req.body.processTime;
+        const travelTime = req.body.travelTime;
+        const bufferTime = req.body.bufferTime;
+        const planType = req.body.planType;
+        const priority = req.body.priority;
+        const place = req.body.place;
+        const isRequiredPlan = req.body.isRequiredPlan;
+        const todoStartTime = req.body.todoStartTime;
 
-        const result = await pool.query(
-            'INSERT INTO plans (\
+        try {
+            const result = await pool.query(
+                'INSERT INTO plans (\
                 user_id, title, context, date, start_time, end_time, process_time, travel_time, buffer_time, plan_type, \
                 priority, place, is_required_plan, todo_start_time) \
                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *',
-            [
-                userId,
-                title,
-                context,
-                date,
-                startTime,
-                endTime,
-                processTime,
-                travelTime,
-                bufferTime,
-                planType,
-                priority,
-                place,
-                isRequiredPlan,
-                todoStartTime
-            ]
-        );
+                [
+                    userId,
+                    title,
+                    context,
+                    date,
+                    startTime,
+                    endTime,
+                    processTime,
+                    travelTime,
+                    bufferTime,
+                    planType,
+                    priority,
+                    place,
+                    isRequiredPlan,
+                    todoStartTime
+                ]
+            );
 
-        return res.status(200).json({
-            isError: false,
-            plan: result.rows[0]
-        });
-    } catch (e) {
-        // TODO バリデーションエラーはHTTPステータスコード400で返却するように実装する
-
-        console.error(e);
-
-        return res.status(500).json({
-            isError: true,
-            errorId: 'errorId',
-            errorMessage: 'システムエラー'
-        });
+            return res.status(200).json({
+                isError: false,
+                plan: result.rows[0]
+            });
+        } catch (e) {
+            console.error(e);
+            return res.status(500).json({
+                isError: true,
+                errorId: 'errorId',
+                errorMessage: 'システムエラー'
+            });
+        }
     }
-});
+);
 
 /**
  * TODO並び順の作成/更新処理
  */
-router.post('/upsertTodoPriority', async (req, res) => {
+router.post('/upsertTodoPriority', [todoOrdersValidationCheck.todoOrders], async (req, res) => {
+    const result = validationResult(req);
+    if (result.errors.length !== 0) {
+        console.error(result);
+        return res.status(400).json({
+            isError: true,
+            errorId: 'errorId',
+            errorMessage: errorMessageFormatter(result.errors)
+        });
+    }
+
     const todoOrders = req.body.todoOrders; // TODOのIDをカンマ区切りにした文字列
     let upsertResult;
 
     try {
-        // TODO バリデーションチェックを行う
-
         // TODO並び順の取得（履歴用ではなく、ユーザーに一つだけ紐づく並び順を取得）
         const getResult = await pool.query('SELECT * FROM todo_orders WHERE user_id = $1 AND schedule_id IS NULL', [
             req.user.id
@@ -99,9 +132,6 @@ router.post('/upsertTodoPriority', async (req, res) => {
         });
     } catch (e) {
         console.error(e);
-
-        // TODO バリデーションエラーはHTTPステータスコード400で返却する
-
         return res.status(500).json({
             isError: true,
             errorId: 'errorId',
