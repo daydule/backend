7354524456f9e353d249bfaf6dcb4e0c4a66b30b
Mellbrother <56198902+Mellbrother@users.noreply.github.com>
2023-01-10 21:14:02 +0900

    DAYD-640 ACT-SCH-ScheduleRead_スケジュール参照 (#15)
    * update DB生成ファイル作成、不要なDB処理削除
    
    * db用ファイル名修正
    
    * スケジュール参照処理実装
    
    * 不要なコメント削除
    
    * fix サンプルデータ、コメント追加
    
    * fix モックデータ、変数名、エラーハンドリング処理修正
    
    Co-authored-by: Keisuke Nemura <nemuraystk@gmail.com>

--- postgres/src/01_ddl/10003_create_table_schedules.sql
+++ postgres/src/01_ddl/10003_create_table_schedules.sql
@@ -2,7 +2,7 @@ CREATE TABLE "schedules"
 (
   "id"                      SERIAL             NOT NULL PRIMARY KEY,
   "user_id"                 INT                NOT NULL REFERENCES "users" ("id"),
-  "date"                    DATE        UNIQUE NOT NULL,
+  "date"                    DATE               NOT NULL,
   "start_time"              VARCHAR(4)         NOT NULL DEFAULT '0900',
   "end_time"                VARCHAR(4)         NOT NULL DEFAULT '1800',
   "start_time_at_schedule"  VARCHAR(4),
