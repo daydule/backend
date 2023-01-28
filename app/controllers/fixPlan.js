dbb88f54e75db12ac63af65a5816a9f0fdc4303f
tom-takeru <tom.2461.ty@gmail.com>
2023-01-28 17:00:37 +0900

    update 仕様変更を反映

--- app/controllers/fixPlan.js
+++ app/controllers/fixPlan.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db/pool');
 const guestCheck = require('../middlewares/guestCheck');
+const format = require('pg-format');
 
 router.use(guestCheck);
 
@@ -12,6 +13,7 @@ router.use(guestCheck);
  */
 router.post('/create', async (req, res) => {
     const dayIds = req.body.dayIds;
+    const setId = req.body.setId;
     const title = req.body.title;
     const context = req.body.context;
     const startTime = req.body.startTime;
@@ -28,40 +30,49 @@ router.post('/create', async (req, res) => {
     try {
         // TODO: バリデーションチェックを行う
         client.query('BEGIN');
-        const ids = [];
-        for (let i = 0; i < dayIds.length; i++) {
-            const sql =
-                'INSERT INTO fix_plans (\
-                day_id, title, context, start_time, end_time, process_time, \
-                travel_time, buffer_time, plan_type, priority, place) \
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id';
-            const values = [
-                dayIds[i],
-                title,
-                context,
-                startTime,
-                endTime,
-                processTime,
-                travelTime,
-                bufferTime,
-                planType,
-                priority,
-                place
-            ];
-            const result = await client.query(sql, values);
-            ids.push(result.rows[0].id);
-        }
 
-        // fix_plans.set_idに、上で作成した固定予定の先頭のidを利用する
-        const result = await client.query(
+        // TODO: db一括挿入をutilsにする
+        const tableName = 'fix_plans';
+        const tableColumns = [
+            'day_id',
+            'title',
+            'context',
+            'start_time',
+            'end_time',
+            'process_time',
+            'travel_time',
+            'buffer_time',
+            'plan_type',
+            'priority',
+            'place'
+        ];
+
+        const values = dayIds.reduce(
+            (previous, id) => [
+                ...previous,
+                [id, title, context, startTime, endTime, processTime, travelTime, bufferTime, planType, priority, place]
+            ],
+            []
+        );
+        const sql = format(
+            'INSERT INTO ' + tableName + ' (' + tableColumns.join(', ') + ') VALUES %L RETURNING *',
+            values
+        );
+
+        const insertResult = await client.query(sql);
+        const ids = insertResult.rows.map((row) => row.id);
+
+        // setIdがリクエストにない時、上で作成した固定予定の先頭のidを利用する
+        const setIdToInsert = setId ? setId : ids[0];
+        const updateResult = await client.query(
             'UPDATE fix_plans SET set_id = $1 WHERE id = ANY($2::INTEGER[]) RETURNING *',
-            [ids[0], ids]
+            [setIdToInsert, ids]
         );
 
         client.query('COMMIT');
         return res.status(200).json({
             isError: false,
-            fixPlans: result.rows
+            fixPlans: updateResult.rows
         });
     } catch (e) {
         client.query('ROLLBACK');
