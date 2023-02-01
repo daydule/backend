f30f36251d8763b64f7b54229ef75cd4ba7d9448
Mellbrother <56198902+Mellbrother@users.noreply.github.com>
2023-02-01 20:58:06 +0900

    DAYD-667 HLP-SCH-SimpleScheduleCreate_シンプルスケジュール作成 (#23)
    * add シンプルスケジュール作成ロジック実装
    
    * 非同期処理追加、単体テストコード実装
    
    * fix 時間不正の場合の戻り値修正
    
    * update 時間に関する処理をutilsに移動、TODO登録時に開始/終了時間を設定するように修正
    
    * fix レビュー箇所修正
    
    * fix docker設定ファイルの誤ったコミットを修正
    
    * fix 変数名修正
    
    * fix テストが正しく実行されるように修正
    
    * fix 英文ミスとスペルミスを修正
    
    * add プロジェクト固有の辞書リストを追加
    
    * fix getStartAndEndTimeStrのreturnのjsDocを修正
    
    * fix リファクタリング＆エラー時レスポンス修正
    
    * update 変数名修正
    
    ---------
    
    Co-authored-by: Keisuke Nemura <nemuraystk@gmail.com>
    Co-authored-by: tom-takeru <57748294+tom-takeru@users.noreply.github.com>
    Co-authored-by: tom-takeru <tom.2461.ty@gmail.com>

--- app/config/const.js
+++ app/config/const.js
@@ -11,6 +11,10 @@ module.exports = {
             SCHEDULE_END_TIME: '1800'
         }
     },
+    SCHEDULE_LOGIC_FILENAME: {
+        0: 'simpleScheduleHelper'
+    },
+    SECTION_MINUTES_LENGTH: 5,
     DAY_LIST: ['日', '月', '火', '水', '木', '金', '土'],
     PLAN_TYPE: {
         PLAN: 0,
---
+++ test/helpers/scheduleHelper.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// eslint-disable-next-line node/no-unpublished-require
+const assert = require('chai').assert;
+// eslint-disable-next-line node/no-unpublished-require
+const sinon = require('sinon');
+// eslint-disable-next-line node/no-unpublished-require
+const proxyquire = require('proxyquire');
+
+const scheduleLogic = {
+    execute: sinon.stub().callsFake((pool, scheduleLogicId, scheduleId, startTime, endTime, plans, todos) => {
+        return 'result';
+    })
+};
+
+const scheduleHelper = proxyquire('../../app/helpers/scheduleHelper', {
+    './schedule/simpleScheduleHelper': scheduleLogic
+});
+
+describe('scheduleHelper.js', function () {
+    describe('createSchedule function', function () {
+        it('should return object with isError flag true when scheduleLogicId is NOT correct.', async function () {
+            const expect = {
+                isError: false,
+                result: 'result'
+            };
+
+            const result = await scheduleHelper.createSchedule('', 0, '', '', '', '', '', '');
+            assert.deepStrictEqual(result, expect);
+        });
+
+        it('should return object with isError flag false when scheduleLogicId is correct.', async function () {
+            const expect = {
+                isError: true,
+                errorId: 'errorId',
+                errorMessage: 'システムエラー'
+            };
+
+            const result = await scheduleHelper.createSchedule('', 1, '', '', '', '', '', '');
+            assert.deepStrictEqual(result, expect);
+        });
+    });
+});
