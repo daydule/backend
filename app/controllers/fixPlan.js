1c1a070290b85ef2081cf8aa404452d051f6e760
tom-takeru <57748294+tom-takeru@users.noreply.github.com>
2023-02-12 13:37:40 +0900

    [PR#32マージ後レビュー]DAYD-718 固定予定更新, 固定予定削除 (#34)
    * DAYD-669 add 固定予定登録APIを追加
    
    * update 仕様変更を反映
    
    * add user辞書に単語を追加
    
    * add 固定予定更新API
    
    * add 固定予定削除API
    
    * add 固定予定削除API用のテストデータを追加
    
    * fix values作る時にmapで書くように修正
    
    * add セットIDによる分岐を追加、レコード一括挿入のutilsを追加
    
    ---------
    
    Co-authored-by: Keisuke Nemura <nemuraystk@gmail.com>

--- app/controllers/fixPlan.js
+++ app/controllers/fixPlan.js
@@ -120,4 +120,89 @@ router.post('/create', async (req, res) => {
     }
 });
 
+/**
+ * 固定予定更新
+ */
+router.post('/update', async (req, res) => {
+    const setId = req.body.setId;
+    const title = req.body.title;
+    const context = req.body.context;
+    const startTime = req.body.startTime;
+    const endTime = req.body.endTime;
+    const processTime = req.body.processTime;
+    const travelTime = req.body.travelTime;
+    const bufferTime = req.body.bufferTime;
+    const priority = req.body.priority;
+    const place = req.body.place;
+
+    const client = await pool.connect();
+    try {
+        // TODO: バリデーションチェックを行う
+        client.query('BEGIN');
+
+        const result = await client.query(
+            'UPDATE fix_plans SET title = $1, context = $2, start_time = $3, end_time = $4, process_time = $5, travel_time = $6, buffer_time = $7, priority = $8, place = $9 \
+            WHERE set_id = $10 RETURNING *',
+            [title, context, startTime, endTime, processTime, travelTime, bufferTime, priority, place, setId]
+        );
+
+        client.query('COMMIT');
+        return res.status(200).json({
+            isError: false,
+            fixPlans: result.rows
+        });
+    } catch (e) {
+        client.query('ROLLBACK');
+        console.error(e);
+        return res.status(500).json({
+            isError: true,
+            errorId: 'errorId',
+            errorMessage: 'システムエラー'
+        });
+    } finally {
+        client.release();
+    }
+});
+
+/**
+ * 固定予定削除
+ */
+router.delete('/delete', async (req, res) => {
+    const ids = req.body.ids;
+
+    const client = await pool.connect();
+    try {
+        // TODO: バリデーションチェックを行う
+        client.query('BEGIN');
+        const result = await client.query('SELECT * FROM fix_plans WHERE id = ANY($1::INTEGER[])', [ids]);
+        if (result.rows.length !== ids.length) {
+            throw new Error('There is some ids that is not existing in fix_plans. ids(' + ids.join(', ') + ')');
+        } else if (result.rows.some((row) => result.rows[0].set_id !== row.set_id)) {
+            throw new Error(
+                'There is some records that has another set_id. ids(' +
+                    ids.join(', ') +
+                    '), set_ids(' +
+                    result.rows.map((row) => row.set_id).join(', ') +
+                    ')'
+            );
+        }
+
+        await client.query('DELETE FROM fix_plans WHERE id = ANY($1::INTEGER[])', [ids]);
+        client.query('COMMIT');
+        return res.status(200).json({
+            isError: false
+        });
+    } catch (e) {
+        client.query('ROLLBACK');
+        console.error(e);
+        return res.status(500).json({
+            isError: true,
+            errorId: 'errorId',
+            errorMessage: 'システムエラー'
+        });
+    } finally {
+        client.release();
+    }
+});
+
 module.exports = router;
