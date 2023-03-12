0324d0e8569c6e7c13a7df0868ccfcdbfcaa0a1d
tom-takeru <tom.2461.ty@gmail.com>
2023-03-12 17:53:42 +0900

    DAYD-639 fix generalValidatorのテストにおいてnext内でのexpectを最後にするように修正

--- test/middlewares/validation/generalValidators.js
+++ test/middlewares/validation/generalValidators.js
@@ -10,27 +10,16 @@ const { plansValidators, errorMessageFormatter } = require('../../../app/middlew
 describe('generalValidators.js', function () {
     describe('plansValidators.title function', function () {
         it('should return error if request includes invalid body.', async function () {
+            const req = {
+                body: {}
+            };
             const expectedError = [
                 '(title : undefined) not empty',
                 '(title : undefined) should be between 1 and 100 characters.'
             ];
-            const next = () => {
-                const result = validationResult(req);
-                expect(expectedError).to.eql(errorMessageFormatter(result.errors));
-            };
-            const req = {
-                body: {}
-            };
-            const res = {
-                status: function (code) {
-                    return {
-                        json: function (json) {
-                            return json;
-                        }
-                    };
-                }
-            };
-            const result = await plansValidators.title(req, res, next);
+            await plansValidators.title(req, {}, () => {});
+            const result = validationResult(req);
+            expect(expectedError).to.eql(errorMessageFormatter(result.errors));
         });
     });
 });
