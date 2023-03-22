e95d17af0aae4895552a2932169ee64a2fcf763f
tom-takeru <tom.2461.ty@gmail.com>
2023-03-22 20:40:27 +0900

    DAYD-639 add errorMessageFormatterにJSDocを追加

--- app/helpers/validationHelper.js
+++ app/helpers/validationHelper.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/**
+ * @param {object[]} errors - バリデーションチェックの結果のエラーオブジェクト配列
+ * @returns {string[]} - バリデーションエラーメッセージの配列
+ */
+
 const errorMessageFormatter = function (errors) {
     return errors.map((error) => {
         return '(' + error.param + ' : ' + error.value + ') ' + error.msg;
