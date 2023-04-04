3795ca1caf57d713e49adfc03239beb13ee1904b
Keisuke Nemura <nemuraystk@gmail.com>
2023-04-04 22:51:52 +0900

    update パスワードと確認用パスワードのバリデーションを追加

--- app/middlewares/validator/authControllerValidators.js
+++ app/middlewares/validator/authControllerValidators.js
@@ -3,7 +3,11 @@
 const { userValidators } = require('./definition/allValidators');
 const validationResultCheck = require('./definition/validationResultCheck');
 
-const signupValidators = [userValidators.email, userValidators.password('password')].concat(validationResultCheck);
+const signupValidators = [
+    userValidators.email,
+    userValidators.password('password'),
+    userValidators.checkSamePassword('password', 'passwordConfirmation')
+].concat(validationResultCheck);
 
 module.exports = {
     signupValidators
