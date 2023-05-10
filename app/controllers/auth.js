602f078314ec95a66ac8309f7d5231147fda3b72
Mellbrother <56198902+Mellbrother@users.noreply.github.com>
2023-05-10 21:25:39 +0900

    add ゲスト初期化処理を実装 (#53)
    Co-authored-by: Keisuke Nemura <nemuraystk@gmail.com>
    Co-authored-by: tom-takeru <57748294+tom-takeru@users.noreply.github.com>

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -131,4 +131,64 @@ router.get('/authError', (req, res) => {
     });
 });
 
+/**
+ * ゲストのサインアップ
+ */
+router.post('/guest/signup', async (req, res) => {
+    const constant = require('../config/const');
+    const now = Date.now();
+
+    const salt = crypto.randomBytes(16).toString('base64');
+    const email =
+        constant.GUEST_INIT.GUEST_USERNAME +
+        now +
+        salt.substring(0, 5) +
+        constant.GUEST_INIT.AT_MARK +
+        constant.GUEST_INIT.GUEST_DOMAIN;
+    const password = constant.GUEST_INIT.GUEST_PASSWORD;
+    const isGuest = true;
+
+    const client = await pool.connect();
+
+    try {
+        client.query('BEGIN');
+
+        const checkEmailExistenceResult = await dbHelper.query(client, 'SELECT * FROM users WHERE email = $1', [email]);
+        if (checkEmailExistenceResult.rows.length > 0) {
+            throw new Error('Email is already registered.');
+        }
+
+        const hashedPassword = await promisify(crypto.pbkdf2)(password, salt, 310000, 32, 'sha256');
+
+        const sql = 'INSERT INTO users (email, hashed_password, salt, is_guest) VALUES ($1, $2, $3, $4) RETURNING *';
+        const values = [email, hashedPassword.toString('base64'), salt, isGuest];
+
+        const guestSignupResult = await dbHelper.query(client, sql, values);
+        await client.query('COMMIT');
+        req.login(guestSignupResult.rows[0], (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({
+                    isError: true,
+                    errorId: 'ServerError',
+                    errorMessage: '予期せぬエラーが発生しました。時間を置いて、もう一度お試しください。'
+                });
+            }
+            return res.status(200).json({
+                isError: false
+            });
+        });
+    } catch (e) {
+        client.query('ROLLBACK');
+        console.error(e);
+        return res.status(500).json({
+            isError: true,
+            errorId: 'ServerError',
+            errorMessage: '予期せぬエラーが発生しました。時間を置いて、もう一度お試しください。'
+        });
+    } finally {
+        client.release();
+    }
+});
+
 module.exports = router;
